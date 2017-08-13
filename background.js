59d34d35aa37f5af0943b6594c251dfd56aedf12
georges <gderemusat@gmail.com>
2017-08-13 10:14:23 +0200

    fix sélection

--- background.js
+++ background.js
@@ -2,12 +2,19 @@ chrome.browserAction.onClicked.addListener((tab) => {
 	chrome.tabs.executeScript({
 		code: `
 		document.querySelectorAll('.js-tweet-text-container > p').forEach(tweet => {
-			const words = tweet.innerHTML.split(' ')
-				.filter(word => word.match(/^[a-z0-9éèàîïâäêë\?\!\'\"\,\.]+$/i))
-				.filter(word => word !== "covfefe");
-			const word = words[Math.floor(Math.random() * words.length)];
-			tweet.innerHTML = tweet.innerHTML.replace(word, "covfefe");
+			const words = tweet.textContent.split(' ')
+				.filter(word => word !== "covfefe" && word !== "");
+			if (words.length > 0) {
+				const text = " covfefe "
+				const word = escapeRegExp(words[Math.floor(Math.random() * words.length)]);
+				const regex = new RegExp('( |^)'+word+'( |$)', 'i');
+				tweet.innerHTML = tweet.innerHTML.replace(regex, text).trim();
+			}
 		});
+
+		function escapeRegExp(str) {
+			return str.replace(/[\\-\\[\\]\\/\\{\\}\\(\\)\\*\\+\\?\\.\\\^\\$\\|]/g, "\\$&");
+		}
 		`
 	});
 });
